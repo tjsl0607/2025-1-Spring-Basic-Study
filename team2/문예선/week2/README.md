d22220bd89fabb13d32c3923939050fbd3a5f848
tjsl0607 <munyeseon@sookmyng.ac.kr>
2025-04-01 05:07:35 +0900

    Feat: 2주차 실습 코드 및 정리

---
+++ team2/문예선/week2/README.md
@@ -0,0 +1,48 @@
+# 2025-1-Spring-Basic-Study
+[APPS] 2025-1 'Spring-Basic' 스터디 저장소🖥️
+
+<br>
+
+## 스터디원 및 정기회의 일정
+- 1팀) 매주 월 10 : 30 - 김지민, 신정은, 엄다현, 우채아 (팀장: 우채아)
+- 2팀) 매주 수 9 : 00 - 방지희, 문예선, 김도연, 윤소영, 정지은 (팀장 : )
+  
+<br>
+
+## 스터디 규칙
+- 매주 **정기 회의 전날 자정**까지 해당 주 학습내용(내용 정리, 실습 코드) PR을 보냅니다.
+- 아래의 `폴더 구조`, `Commit 규칙`, `PR 규칙`을 준수해주세요.
+
+<br>
+
+## 폴더 구조
+- `teamN`/`이름`/`weekN`/파일
+    - ex) team1/홍길동/week1/Controller.java
+ 
+<br>
+
+## Commit 규칙
+- Commit Message는 다음을 참고합니다.
+  - Feat: 기능 추가, 실습, 과제 코드 파일이나 부수적인 코드 추가
+  - Fix: 버그 및 코드 수정
+  - Docs: 문서 수정 (README.md 등)
+  - Design: CSS 등 사용자 UI 디자인 변경
+  - Move: 프로젝트 파일 및 코드 이동
+  - Remove: 불필요한 코드 및 파일을 삭제
+  - Rename: 파일 및 폴더명을 변경
+  - Refactor: 코드 리팩터링
+  - Comment: 필요한 주석 추가 및 변경
+  - Release: 버전 릴리즈
+- ex)
+  - Feat: 6주차 과제 및 실습 코드
+  - Fix: 4주차 과제 index.html 문법 오류 수정
+  - Move: 5주차 파일 이동
+
+<br>
+
+## PR(Pull Request) 규칙
+- 이 repository를 fork해서 본인 이름으로 만든 폴더에서 작업하고 commit후 Pull request를 보냅니다.
+- PR은 각 주차별 학습내용(내용 정리, 실습 코드) commit을 함께 보냅니다.
+- 제목은 `[n주차 Spring-Basic 스터디] 이름`으로 설정합니다.
+  - ex) [2주차 Spring-Basic 스터디] 홍길동
+- 내용은 어려웠던 점과 배운 점 등 자유롭게 작성합니다.
---
+++ team2/문예선/week2/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,40 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+        // 리포지토리 외부에서 직접 넣어줌: Dependency Injection
+    }
+
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) { //이름 중복확인
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재합니다.");
+                });
+    }
+
+    public List<Member> findMembers() { //전체 회원조회
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) { //아이디로 회원 찾기
+        return memberRepository.findById(memberId);
+    }
+}
